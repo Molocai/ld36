109e96bd6e8a05031adfd3f0ffafe118a4e8ffd6
Brindibou <molocai@molocai.com>
2016-08-27 17:30:00 +0200

    Keybindings

---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LD36
+{
+    public class MapManager : MonoBehaviour
+    {
+        public GameObject[] spawns;
+        public GameObject playerPrefab;
+
+        void Start()
+        {
+            int nbPlayers = 4; //GameManager.Get.NbPlayers;
+            for (int i = 0; i < nbPlayers; i++)
+            {
+                GameObject player = Instantiate(playerPrefab, spawns[i].transform.position, Quaternion.identity) as GameObject;
+                PlayerController playerController = player.GetComponentInChildren<PlayerController>();
+                playerController.playerInputs = GameManager.Get.keybindings[i];
+            }
+        }
+    }
+}
