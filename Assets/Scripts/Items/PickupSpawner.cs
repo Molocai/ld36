84ce709de3d8541d501745767bacd71ca01b423b
Brindibou <molocai@molocai.com>
2016-08-28 01:06:50 +0200

    Item spawner

--- Assets/Scripts/Items/PickupSpawner.cs
+++ Assets/Scripts/Items/PickupSpawner.cs
@@ -5,29 +5,25 @@ namespace LD36
 {
     public class PickupSpawner : MonoBehaviour
     {
-        [System.Serializable]
-        public struct Spawner
-        {
-            public GameObject item;
-            public int weight;
-        }
-
-        public Spawner[] spawners;
+        public int qtyToSpawn;
+        public GameObject[] pickups;
+        public float spawnRadius;
 
         // Use this for initialization
         void Start()
         {
-            int total = 0;
-            foreach(Spawner s in spawners)
+            for (int i = 0; i < qtyToSpawn; i++)
             {
-                total += s.weight;
+                GameObject spawn = pickups[Random.Range(0, pickups.Length)];
+                Vector2 position = Random.insideUnitCircle * spawnRadius;
+                Instantiate(spawn, transform.position + new Vector3(position.x, position.y), Quaternion.identity);
             }
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnDrawGizmos()
         {
-
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
         }
     }
 }
