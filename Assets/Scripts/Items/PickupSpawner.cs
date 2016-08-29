587bf7cf80487aade9c577904c31fa8ca3613a79
Brindibou <molocai@molocai.com>
2016-08-29 11:34:32 +0200

    Poids sur les spawn d'objet

--- Assets/Scripts/Items/PickupSpawner.cs
+++ Assets/Scripts/Items/PickupSpawner.cs
@@ -5,19 +5,43 @@ namespace LD36
 {
     public class PickupSpawner : MonoBehaviour
     {
+        [System.Serializable]
+        public struct SpawnGroup
+        {
+            public int weight;
+            public GameObject item;
+            [HideInInspector()]
+            public int cumulativeWeight;
+        }
+
+        public SpawnGroup[] itemsToSpawn;
         public int qtyToSpawn;
-        public GameObject[] pickups;
         public float spawnRadius;
 
         // Use this for initialization
         void Start()
         {
+            int total = 0;
+
+            for (int i = 0; i < itemsToSpawn.Length; i++)
+            {
+                total += itemsToSpawn[i].weight;
+                itemsToSpawn[i].cumulativeWeight = total;
+            }
+
             for (int i = 0; i < qtyToSpawn; i++)
             {
-                GameObject spawn = pickups[Random.Range(0, pickups.Length)];
-                Vector2 position = Random.insideUnitCircle * spawnRadius;
-                GameObject item = Instantiate(spawn, transform.position + new Vector3(position.x, position.y), Quaternion.identity) as GameObject;
-                item.transform.localScale = new Vector3((Random.Range(0, 2) == 0) ? spawn.transform.localScale.x * -1 : spawn.transform.localScale.x, spawn.transform.localScale.y);
+                int rand = Random.Range(0, total + 1);
+                foreach (SpawnGroup s in itemsToSpawn)
+                {
+                    if (s.cumulativeWeight >= rand)
+                    {
+                        Vector2 position = Random.insideUnitCircle * spawnRadius;
+                        GameObject item = Instantiate(s.item, transform.position + new Vector3(position.x, position.y), Quaternion.identity) as GameObject;
+                        item.transform.localScale = new Vector3((Random.Range(0, 2) == 0) ? s.item.transform.localScale.x * -1 : s.item.transform.localScale.x, s.item.transform.localScale.y);
+                        break;
+                    }
+                }
             }
         }
 
